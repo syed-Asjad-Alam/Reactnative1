822647f8515b8f9c8e3af0fc9d9d250e85ff827e
Syed Asjad Alam <asjadakmal1@gmail.com>
2021-10-27 21:39:35 +0500

    adasd

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Modal, StyleSheet, Text, View,Pressable } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.centeredView}>
+      <Modal animationType="none" visible={true}>
+      <View style={styles.centeredView}>
+        <View style={styles.centeredView,{flexDirection:'row'}}>
+          <View style={styles.modalView}>
+
+          </View>
+          <View style={styles.modalView}>
+
+          </View>
+          <View style={styles.modalView}>
+
+          </View>
+        </View>
+        <View style={styles.centeredView,{flexDirection:'row'}}>
+          <View style={styles.modalView}>
+
+          </View>
+          <View style={styles.modalView}>
+
+          </View>
+          <View style={styles.modalView}>
+
+          </View>
+        </View>
+        <View style={styles.centeredView,{flexDirection:'row'}}>
+          <View style={styles.modalView}>
+
+          </View>
+          <View style={styles.modalView}>
+
+          </View>
+          <View style={styles.modalView}>
+
+          </View>
+        </View>
+      </View>
+        
+        
+      </Modal>
+      
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  centeredView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
   },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  
+  
 });
+export default App
