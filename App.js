58bed361cc21aab153c996f9df1657ddb3da25b4
Syed Asjad Alam <asjadakmal1@gmail.com>
2021-10-27 22:30:27 +0500

    Task 2 done

--- App.js
+++ App.js
@@ -1,53 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Modal, StyleSheet, Text, View,Pressable } from 'react-native';
+import { Modal, StyleSheet, Text, View ,Button, Pressable} from 'react-native';
 
+
+const MainComp = (props) => {
+  const [playernumber,setplayernumber] = React.useState(1)
+  return (
+      <View style={styles.centeredView}>
+        <Modal animationType="none" visible={true}>
+        <View style={styles.centeredView}>
+          <Text>Player {playernumber}</Text>
+          <Text>Player {playernumber}'s symbol {props.title} </Text>
+          <View style={styles.centeredView,{flexDirection:'row'}}>
+            <View style={styles.modalView}>
+              <Pressable></Pressable>
+  
+            </View>
+            <View style={styles.modalView}>
+              <Pressable></Pressable>
+  
+            </View>
+            <View style={styles.modalView}>
+              <Pressable></Pressable>
+  
+            </View>
+          </View>
+          <View style={styles.centeredView,{flexDirection:'row'}}>
+            <View style={styles.modalView}>
+              <Pressable></Pressable>
+  
+            </View>
+            <View style={styles.modalView}>
+              <Pressable></Pressable>
+  
+            </View>
+            <View style={styles.modalView}>
+              <Pressable></Pressable>
+  
+            </View>
+          </View>
+          <View style={styles.centeredView,{flexDirection:'row'}}>
+            <View style={styles.modalView}>
+              <Pressable></Pressable>
+  
+            </View>
+            <View style={styles.modalView}>
+              <Pressable></Pressable>
+  
+            </View>
+            <View style={styles.modalView}>
+              <Pressable></Pressable>
+  
+            </View>
+          </View>
+        </View>
+          
+          
+        </Modal>
+        
+       
+      </View>
+    );
+
+}
 const App = () => {
+  const [isStarted, setStart] = React.useState(false)
+  const [Playersymbol, setPlayerSymbol] = React.useState()
   return (
     <View style={styles.centeredView}>
-      <Modal animationType="none" visible={true}>
-      <View style={styles.centeredView}>
-        <View style={styles.centeredView,{flexDirection:'row'}}>
-          <View style={styles.modalView}>
-
-          </View>
-          <View style={styles.modalView}>
-
-          </View>
-          <View style={styles.modalView}>
-
-          </View>
+      <Text>Tic Tac Toe Game</Text>
+      <Text>Select Your Symbol</Text>
+      <View style={{flexDirection:'row',margin:10}}>
+        <View style={{width:100,margin:20}}>
+          {!isStarted && <Button title="O" onPress={() => {
+            setStart(true)
+            setPlayerSymbol("O")}} />}
         </View>
-        <View style={styles.centeredView,{flexDirection:'row'}}>
-          <View style={styles.modalView}>
-
-          </View>
-          <View style={styles.modalView}>
-
-          </View>
-          <View style={styles.modalView}>
-
-          </View>
-        </View>
-        <View style={styles.centeredView,{flexDirection:'row'}}>
-          <View style={styles.modalView}>
-
-          </View>
-          <View style={styles.modalView}>
-
-          </View>
-          <View style={styles.modalView}>
-
-          </View>
+        <View style={{width:100,margin:20}}>
+          {!isStarted && <Button title="X" onPress={() => {
+            setStart(true)
+            setPlayerSymbol("X")}} />}
         </View>
       </View>
-        
-        
-      </Modal>
-      
-     
+      {isStarted && <MainComp title={Playersymbol} />}
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
